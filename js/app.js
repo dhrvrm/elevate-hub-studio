9d2cf86160ecfb7ead3f2b42781a9d451bdd4672
dhrvrm <dhruvverma028@gmail.com>
2024-03-12 00:07:17 +0530

    Add background and type animation

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const colors = ["#ffcf00", "#007087", "#EC994B", "#C70039"];
+const span = document.querySelector(".typing");
+var cursor = 0;
+let changeColor = () => {
+  if (cursor >= colors.length) {
+    cursor = 0;
+  }
+  span.style.color = colors[cursor++];
+};
+var options = {
+  strings: [
+    "that grow",
+    "that elevate",
+    "that enagage",
+    "that empower",
+    "that inspire",
+    "that impact",
+    "that innovate",
+    "that impact",
+  ],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 600,
+  smartBackspace: false,
+  loop: true,
+  preStringTyped: (arrayPos, self) => changeColor(),
+  onLastStringBackspaced: (self) => changeColor(),
+  // onLastStringBackspaced: (self) => changeColor(),
+};
+
+try {
+  var typed = new Typed(".typing-span", options);
+  AOS.init();
+} catch (e) {
+  console.log("initialization error");
+}
